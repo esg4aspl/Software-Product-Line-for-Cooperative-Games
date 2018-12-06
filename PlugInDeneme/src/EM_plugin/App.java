6026d15a5edb401f7474cda309cf9eed741dbee6
Elif Yılmaz <Elif Ylmaz@10.8.43.232>
2018-12-07 00:15:09 +0300

    Second Commit.

--- PlugInDeneme/src/EM_plugin/App.java
+++ PlugInDeneme/src/EM_plugin/App.java
@@ -1,5 +1,13 @@
 package EM_plugin;
 
+import java.io.FileNotFoundException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+
+import EM_Config.ConfigWriter;
 import EM_plugin.Generator.DiceGenerator;
 import EM_plugin.Generator.FigureGenerator;
 import EM_plugin.Generator.GeneratorPlugInI;
@@ -8,7 +16,8 @@ import EM_plugin.Generator.PlayerGenerator;
 
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InvalidFormatException, ParserConfigurationException, TransformerException, FileNotFoundException {
+		/*
 		GameConfig game = new GameConfig();
 		
 		GeneratorPlugInI players = new PlayerGenerator(2);
@@ -22,7 +31,10 @@ public class App {
 		game.registerPlugIn(figures);
 		game.start();
 		
-	
+		*/
+		
+		ConfigWriter configWriter = new ConfigWriter();
+	    configWriter.write("Features.txt");
 		
 	}
 
